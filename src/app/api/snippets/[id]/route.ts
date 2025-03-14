a8855d179b175626e6fe70422b56b86dac0441db
matias <you@example.com>
2025-03-14 18:45:18 -0300

    Fix arg param in GET

--- src/app/api/snippets/[id]/route.ts
+++ src/app/api/snippets/[id]/route.ts
@@ -1,13 +1,12 @@
 import { getSnippetFromDb } from "@/utils/restdb";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
-    request: Request,
-    context: { params: { id: string } }
+    request: NextRequest,
+    { params }: { params: { id: string } }
 ) {
     try {
-        const { id } = context.params;
-        
+        const { id } = params;
         const snippet = await getSnippetFromDb(id);
         
         if (!snippet) {
