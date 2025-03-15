d2af60c606a2b2df61569c989d1ec99805226a7d
matias <you@example.com>
2025-03-14 22:00:00 -0300

    Fix bg style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   }, [])
 
   return (
-    <div className="flex text-center flex-col justify-start items-center h-screen bg-[url(/Hero-Background-notecode.svg)] bg-no-repeat bg-start xl:bg-cover">
+    <div className="flex text-center flex-col justify-start items-center h-screen bg-[url(/Hero-Background-notecode.svg)] bg-no-repeat bg-start 2xl:bg-cover">
       <Image className="py-10" src="/NoteCodeLogo.svg" alt="page-logo" width={110} height={20} />
       <h2 className="text-black text-3xl font-bold py-2">Create & Share</h2>
       <h1 className="text-black text-4xl font-bold ">Your Code easily</h1>
